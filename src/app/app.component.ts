81a9c53f123084a3661adf6a8a53b1a615acc1ae
chris yaconiello <cyaconiello@captechconsulting.com>
2016-12-30 14:21:39 -0500

    making the reset button work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,46 @@ export class AppComponent implements OnInit {
 
   toggleForm: boolean;
   counter: number;
-  firstName: string;
-  lastName: string;
+  adjOne: string;
+  pnounTwo: string;
+  pnounThree: string;
+  verbFour: string;
+  nounFive: string;
+  verbSix: string;
+  verbSeven: string;
+  pnounEight: string;
+  adjNine: string;
+  adjTen: string;
+  pnounEleven: string;
+  pnounTwelve: string;
+  occupationThirteen: string;
+  adjFourteen: string;
 
   ngOnInit() {
     this.toggleForm = false;
     this.counter = 0;
-    this.firstName = '';
-    this.lastName = '';
   }
 
   incrementCounter() {
     this.counter++;
   }
 
+  reset() {
+    this.toggleForm = false;
+    this.counter = 0;
+    this.adjOne = '';
+    this.pnounTwo = '';
+    this.pnounThree = '';
+    this.verbFour = '';
+    this.nounFive = '';
+    this.verbSix = '';
+    this.verbSeven = '';
+    this.pnounEight = '';
+    this.adjNine = '';
+    this.adjTen = '';
+    this.pnounEleven = '';
+    this.pnounTwelve = '';
+    this.occupationThirteen = '';
+    this.adjFourteen = '';
+  }
 }
